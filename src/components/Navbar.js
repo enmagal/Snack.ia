7a70c3880e2f5f0c135113b4228b32d86099fd07
enzo-magal <61931855+enzo-magal@users.noreply.github.com>
2024-03-09 23:17:56 +0100

    :sparkles: Create the landing page

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -2,14 +2,24 @@ import React from "react";
 
 function Navbar(props) {
   return (
-    <div>
-      <p>Snack.ia</p>
-      <a href="#">Nos clients</a>
-      <a href="#">Fonctionnalités</a>
-      <a href="#">Prix</a>
+    <div className="w-screen flex justify-between items-center pt-6 pb-6">
+      <div className="flex justify-between w-1/3 pl-6 items-center">
+        <p className="text-2xl font-bold">Snack.ia</p>
+        <a className="text-gray-400 hover:text-gray-700" href="#">
+          Nos clients
+        </a>
+        <a className="text-gray-400 hover:text-gray-700" href="#">
+          Fonctionnalités
+        </a>
+        <a className="text-gray-400 hover:text-gray-700" href="#">
+          Prix
+        </a>
+      </div>
       <div>
-        <button>Connexion</button>
-        <button>Essai gratuit</button>
+        <button className="hover:text-green-500">Connexion</button>
+        <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded mx-6">
+          Essai gratuit
+        </button>
       </div>
     </div>
   );
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,21 +3,28 @@ import Navbar from "../components/Navbar";
 
 function Landing(props) {
   return (
-    <div>
+    <div className="flex flex-col items-center h-screen">
       <Navbar />
-      <h1>
-        Boostez vos ventes <br /> avec <em>Snack.ia</em>
-      </h1>
-      <p>
-        Snack.ia propose une solution d'analyse de données alimentée par
-        l'intelligence artificielle pour les restaurants. Obtenez des insights
-        précis sur les ventes, les tendances de consommation et la rentabilité,
-        afin d'optimiser vos performances et offrir une expérience culinaire
-        exceptionnelle à vos clients
-      </p>
-      <div>
-        <button>👋 Commencer gratuitement</button>
-        <button>🚩 Nos clients</button>
+      <div className="flex-1 flex flex-col items-center pt-10">
+        <h1 className="text-6xl font-bold">
+          Boostez vos ventes <br /> avec{" "}
+          <em className="text-green-500 not-italic">Snack.ia</em>
+        </h1>
+        <p className="w-2/3 my-10">
+          Snack.ia propose une solution d'analyse de données alimentée par
+          l'intelligence artificielle pour les restaurants. Obtenez des insights
+          précis sur les ventes, les tendances de consommation et la
+          rentabilité, afin d'optimiser vos performances et offrir une
+          expérience culinaire exceptionnelle à vos clients
+        </p>
+        <div className="flex justify-around w-1/3">
+          <button className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded shadow duration-300">
+            👋 Commencer gratuitement
+          </button>
+          <button className="border-green-500 text-green-500 hover:border-green-700 font-bold hover:text-green-700 border-2 py-2 px-4 rounded  duration-300 shadow">
+            🚩 Nos clients
+          </button>
+        </div>
       </div>
     </div>
   );
